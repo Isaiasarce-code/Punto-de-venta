91e02791f25118eb21de1670c87b3e6eec253920
Isaiasarce-code <isaiasarce321@gmail.com>
2025-06-18 16:51:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 
 import pandas as pd
 import gspread
